59459206299413f0774561635320265def04d5b7
Pedro Lopez-Cabanillas <plcl@users.sourceforge.net>
2006-12-29 10:28:56 +0000

    updated documentation and copyright note
    git-svn-id: https://svn.code.sf.net/p/kmetronome/code/trunk@37 ea54cc14-8072-40c6-bac7-338dc0ed4500

--- src/kmetronomeview.h
+++ src/kmetronomeview.h
@@ -1,7 +1,7 @@
 /***************************************************************************
  *   KMetronome - ALSA Sequencer based MIDI metronome                      *
- *   Copyright (C) 2005 by Pedro Lopez-Cabanillas                          *
- *   plcl@users.sourceforge.net                                            *
+ *   Copyright (C) 2005-2006 Pedro Lopez-Cabanillas                        *
+ *   <plcl@users.sourceforge.net>                                          *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -14,10 +14,11 @@
  *   GNU General Public License for more details.                          *
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA 02110-1301, USA                                                    *
  ***************************************************************************/
+
 #ifndef KMETRONOMEVIEW_H
 #define KMETRONOMEVIEW_H
 

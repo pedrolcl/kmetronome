6ec1c06be30ad6d009282063deb195898daff9ac
Pedro López-Cabanillas <pedrolcl@users.noreply.github.com>
2026-09-13 12:09:22 +0200

    Revert "avoid crash at exit"
    This reverts commit a1d1c024b0edcfda73ae57df5b9a10fba5e5ffd0.

--- src/sequenceradapter.cpp
+++ src/sequenceradapter.cpp
@@ -69,7 +69,8 @@ SequencerAdapter::SequencerAdapter(QObject *parent) :
     // Using a callback to receive sequencer events:
     m_Client->setHandler(this);
 
-    m_Port = m_Client->createPort();
+    m_Port = new MidiPort(this);
+    m_Port->attach( m_Client );
     m_Port->setPortName(QSTR_APPNAME);
     m_Port->setCapability(SND_SEQ_PORT_CAP_WRITE |
                           SND_SEQ_PORT_CAP_SUBS_WRITE |
@@ -90,7 +91,8 @@ SequencerAdapter::SequencerAdapter(QObject *parent) :
 
 SequencerAdapter::~SequencerAdapter() 
 {
-    m_Client->stopSequencerInput();
+    m_Client->stopSequencerInput();    
+    m_Port->detach();
     m_Client->close();
 }
 
